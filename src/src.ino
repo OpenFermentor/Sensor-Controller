dc9008285d59f03e63dd9e95d1d9935afae8ce7b
Renzo Scuadroni <renzoscuadroni@outlook.com>
2018-02-12 20:57:15 -0300

    Add third pump and support for Atlas Scientific temperature probe

--- src/src.ino
+++ src/src.ino
@@ -6,70 +6,73 @@
 
 // General
 #define SERIAL_BAUD 115200
-#define MAX_INPUT_LENGTH 255
 #define ANALOG_READ_MAX 1023
 #define PH_I2C_ADDRESS 99
+#define TEMP_I2C_ADDRESS 102
 #define SECONDARY_PSU_STATUS_READ_THRESHOLD 512
 
 // Pins
 const byte PUMP_ENABLE[] = {5, 6, 3};
 const byte PUMP_PIN1[] = {8, 10, 12};
 const byte PUMP_PIN2[] = {9, 11, 13};
-#define ONE_WIRE_BUS 2
 #define SECONDARY_PSU_STATUS_PIN A0
 
 // Variables
-char input[MAX_INPUT_LENGTH];
-OneWire oneWire(ONE_WIRE_BUS);
-DallasTemperature sensors(&oneWire);
 // DEFAULT TEMPERATURE FOR PH CALIBRATION
 float temperature = 25;
 
 void setup() {
   Serial.begin(SERIAL_BAUD);
+  Serial.setTimeout(50);
   pinMode(PUMP_ENABLE[0], OUTPUT);
   digitalWrite(PUMP_ENABLE[0], LOW);
   pinMode(PUMP_ENABLE[1], OUTPUT);
   digitalWrite(PUMP_ENABLE[1], LOW);
+  pinMode(PUMP_ENABLE[2], OUTPUT);
+  digitalWrite(PUMP_ENABLE[2], LOW);
   pinMode(PUMP_PIN1[0], OUTPUT);
   digitalWrite(PUMP_PIN1[0], LOW);
   pinMode(PUMP_PIN1[1], OUTPUT);
   digitalWrite(PUMP_PIN1[1], LOW);
+  pinMode(PUMP_PIN1[2], OUTPUT);
+  digitalWrite(PUMP_PIN1[2], LOW);
   pinMode(PUMP_PIN2[0], OUTPUT);
   digitalWrite(PUMP_PIN2[0], LOW);
   pinMode(PUMP_PIN2[1], OUTPUT);
   digitalWrite(PUMP_PIN2[1], LOW);
+  pinMode(PUMP_PIN2[2], OUTPUT);
+  digitalWrite(PUMP_PIN2[2], LOW);
   Wire.begin();
-  // AVOID WRONG READS AND INITIALIZE TEMPERATURE FOR PH CALIBRATION
-  getTemp();
-  getTemp();
 }
 
 void loop() {
   if (Serial.available() > 0) {
-    byte terminatorAt = Serial.readBytes(input, MAX_INPUT_LENGTH);
-    while (input[terminatorAt-1] == '\r' || input[terminatorAt-1] == '\n') {
-      terminatorAt--;
-    }
-    input[terminatorAt] = 0;
-    char* command = strtok(input, " ");
-    if (strcmp(command, "getTemp") == 0) {
+    char command[3];
+    Serial.readBytes(command, 3);
+    command[2] = 0;
+    if (strcmp(command, "GT") == 0) {
       Serial.println(getTemp());
     }
-    else if (strcmp(command, "calibratePh") == 0) {
-      char* type = strtok(0, " ");
+    else if (strcmp(command, "CP") == 0) {
+      char type[2];
+      Serial.readBytes(type, 2);
+      command[1] = 0;
       Serial.println(calibratePh(type));
     }
-    else if (strcmp(command, "getPh") == 0) {
+    else if (strcmp(command, "GP") == 0) {
       Serial.println(getPh());
     }
-    else if (strcmp(command, "getStatus") == 0) {
+    else if (strcmp(command, "GS") == 0) {
       Serial.println(getStatus());
     }
-    else if (strcmp(command, "setPump") == 0) {
-      char* pump = strtok(0, " ");
-      char* movements = strtok(0, " ");
-      Serial.println(setPumpMultiple(pump, movements));
+    else if (strcmp(command, "SP") == 0) {
+      char pump[2];
+      Serial.readBytes(pump, 2);
+      pump[1] = 0;
+      char movements[63];
+      int bytes = Serial.readBytesUntil('\n', movements, 63);
+      movements[bytes] = 0;
+      setPumpMultiple(pump, movements);
     }
     else {
       Serial.println("Unrecognized Command");
@@ -78,26 +81,22 @@ void loop() {
 }
 
 char* getTemp() {
-  sensors.requestTemperatures();
-  float readValue = sensors.getTempCByIndex(0);
-  if (readValue == -127.00) {
+  char* readValue = sendI2C(TEMP_I2C_ADDRESS, "R", 900);
+  if (strcmp(readValue, "-1023.000") == 0) {
     return "ERROR";
   } else {
-    temperature = readValue;
-    char temperatureChar[10];
-    sprintf(temperatureChar, "%f", readValue);
-    return temperatureChar;
+    return readValue;
   }
 }
 
 char* calibratePh(char* type) {
-  if (strcmp(type, "acid") == 0) {
+  if (strcmp(type, "A") == 0) {
     sendI2C(PH_I2C_ADDRESS, "Cal,low,4.00", 900);
     return "OK";
-  } else if (strcmp(type, "neutral") == 0) {
+  } else if (strcmp(type, "N") == 0) {
     sendI2C(PH_I2C_ADDRESS, "Cal,mid,7.00", 900);
     return "OK";
-  } else if (strcmp(type, "base") == 0) {
+  } else if (strcmp(type, "B") == 0) {
     sendI2C(PH_I2C_ADDRESS, "Cal,high,10.00", 900);
     return "OK";
   } else {
@@ -124,11 +123,11 @@ char* getStatus() {
   char* tempStatus = getTempStatus();
   char* secondaryPSUStatus = getSecondaryPSUStatus();
   char statusChar[100];
-  strcpy(statusChar, "SecondaryPSU:");
+  strcpy(statusChar, "SPSU:");
   strcat(statusChar, secondaryPSUStatus);
-  strcat(statusChar, ",PHSENSOR:");
+  strcat(statusChar, ",PS:");
   strcat(statusChar, phStatus);
-  strcat(statusChar, ",TEMPSENSOR:");
+  strcat(statusChar, ",TS:");
   strcat(statusChar, tempStatus);
   return statusChar;
 }
@@ -136,36 +135,36 @@ char* getStatus() {
 char* getSecondaryPSUStatus() {
   int value = analogRead(SECONDARY_PSU_STATUS_PIN);
   if (value < SECONDARY_PSU_STATUS_READ_THRESHOLD) {
-    return "OFF";
+    return "0";
   } else {
-    return "ON";
+    return "1";
   }
 }
 
 char* getPhStatus() {
   char* ph = getPh();
   if (strcmp(ph, "ERROR") == 0) {
-    return "OFF";
+    return "0";
   } else {
-    return "ON";
+    return "1";
   }
 }
 
 char* getTempStatus() {
   char* temp = getTemp();
   if (strcmp(temp, "ERROR") == 0) {
-    return "OFF";
+    return "0";
   } else {
-    return "ON";
+    return "1";
   }
 }
 
-char* setPumpMultiple(char* pump, char* movements) {
+void setPumpMultiple(char* pump, char* movements) {
   char* secondaryPSUStatus = getSecondaryPSUStatus();
-  if (strcmp(secondaryPSUStatus, "OFF") == 0) {
-    return "ERROR";
+  if (strcmp(secondaryPSUStatus, "0") == 0) {
+    Serial.println("ERROR");
   } else {
-    return "OK";
+    Serial.println("OK");
     char* movement = strtok_r(movements, ",", &movements);
     while (movement != 0) {
       char* pumpState = strtok_r(movement, ":", &movement);
